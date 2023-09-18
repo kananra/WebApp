91d0da14907876fbc231794e2d272259face5561
kananra <kananara@code.edu.az>
2023-09-18 14:59:45 +0400

    PaymentBackend created

--- API/Program.cs
+++ API/Program.cs
@@ -76,7 +76,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
      });
 builder.Services.AddAuthorization();
 builder.Services.AddScoped<TokenService>();
-
+builder.Services.AddScoped<PaymentService>();
 
 
 
